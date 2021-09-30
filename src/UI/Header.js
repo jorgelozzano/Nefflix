f0ceed649e3a67d5ddb960ae7ee04bc672f7f15c
Jorge Lozano <ernie.lozano23@gmail.com>
2021-09-30 00:55:34 -0500

    header with BS

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header>
-            <div className="title">
-                <h1>Nefflix</h1>
-            </div>
-            <ul className="header_options">
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </header>
-    )
-}
+  return (
+    <header>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Nefflix</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Home</Link>
+            </li>
+            <li className="navbar-item">
+              <Link className="nav-link">About</Link>
+            </li>
+            <li className="navbar-item">
+              <Link className="nav-link">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
