0e90a7af3f36844f2aae4d9eb38ee5def93e3fbe
Jorge Lozano <ernie.lozano23@gmail.com>
2021-09-30 14:31:39 -0500

    propuesta

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-require('dotenv').config();
+import express from "express";
+import  cors from "cors";
+import restaurants from "./api/restaurants.route.js";
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+app.use("/api/v1/restaurants", restaurants)
+app.use("*", (req,res) => {
+    res.status(404).json({error: "not found"})
 })
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+export default app
