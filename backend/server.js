c2c3ef5592d6f183deb57211eb722a02c20aa8fd
Jorge Lozano <ernie.lozano23@gmail.com>
2021-09-30 12:07:17 -0500

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 require('dotenv').config();
 
@@ -9,14 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const aboutRouter = require("./routes/about");
-// const contactRouter = require("./routes/contact");
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connection established');
+});
 
-// app.use("/contact", contactRouter);
-// app.use("/about", aboutRouter);
-app.get("/", (req,res) => {
-    res.send("Hola")
-})
+const aboutRouter = require("./routes/about");
+const contactRouter = require("./routes/contact");
+
+app.get("/", (req, res) => {
+    res.send("hola mundo");
+});
+app.use("/contact", contactRouter);
+app.use("/about", aboutRouter);
 
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
