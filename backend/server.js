8aefb9ef1b96796a837f47540fc67b80619ca9b6
Jorge Lozano <ernie.lozano23@gmail.com>
2021-09-30 13:09:59 -0500

    mongooseees

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-require('dotenv').config();
-
-const username = "sdJorge";
-const password = "mean123";
-const cluster = "Cluster0";
-const dbname = "myFirstDatabase";
+require('dotenv').config({path: '.env'});
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,22 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(
-    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
-    {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    }
-);
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//     console.log('Database connection established');
-// });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-    console.log("Connected successfully");
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex:  true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connection established');
 });
 
 const aboutRouter = require("./routes/about");
