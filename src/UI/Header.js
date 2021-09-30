0194cf41047c3a660e032024cf1f61874d91d9de
Jorge Lozano <ernie.lozano23@gmail.com>
2021-09-30 01:04:58 -0500

    header updated

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -4,22 +4,25 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Nefflix</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">Home</Link>
-            </li>
-            <li className="navbar-item">
-              <Link className="nav-link">About</Link>
-            </li>
-            <li className="navbar-item">
-              <Link className="nav-link">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <Link className="navbar-brand" to="/">Nefflix</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <Link className="nav-item nav-link active" to="/">Home</Link>
+        <Link className="nav-item nav-link">About</Link>
+        <Link className="nav-item nav-link">Contact</Link>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </header>
   );
 };
