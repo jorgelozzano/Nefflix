b48213a040654a34680d00a82b0bf10127364986
Jorge Lozano <ernie.lozano23@gmail.com>
2021-09-30 00:15:12 -0500

    contact & about routes added

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const aboutRouter = require("./routes/about");
+const contactRouter = require("./routes/contact");
+
+app.use("/contact", contactRouter);
+app.use("/about", aboutRouter);
+
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
 });
