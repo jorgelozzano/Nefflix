17aba15293652738ed3bdd08dae6f2c76ca9d2ee
Jorge Lozano <ernie.lozano23@gmail.com>
2021-09-30 12:54:54 -0500

    mongooseee

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose')
 
 require('dotenv').config();
 
+const username = "sdJorge";
+const password = "mean123";
+const cluster = "Cluster0";
+const dbname = "myFirstDatabase";
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,10 +16,22 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Database connection established');
+mongoose.connect(
+    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }
+);
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log('Database connection established');
+// });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
 });
 
 const aboutRouter = require("./routes/about");
