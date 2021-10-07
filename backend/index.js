19e5a6bdb9a1b5c0d5d7bb40db1491b11724c1ea
Jorge Lozano <ernie.lozano23@gmail.com>
2021-10-07 15:16:39 -0500

    remove newUrlParse

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,7 @@ const port = process.env.PORT || 8000
 
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,{
-        poolSize: 50,
         wtimeout: 2500,
-        useNewUrlParse: true
     }
 ).catch(err => {
     console.error(err.stack)
