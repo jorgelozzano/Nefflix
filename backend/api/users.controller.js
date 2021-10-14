8ef775bc83138ad5a96e79aeda55ac12c9b09ac7
Jorge Lozano <ernie.lozano23@gmail.com>
2021-10-13 23:02:01 -0500

    adding CRUD

--- backend/api/users.controller.js
+++ backend/api/users.controller.js
@@ -1,4 +1,6 @@
 import usersDAO from "../dao/usersDAO.js";
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
 
 export default class UsersController {
     static async apiGetUsers(req, res, next) {
@@ -23,5 +25,63 @@ export default class UsersController {
             total_results: totalNumUsers,
         }
         res.json(response)
+    }
+
+    static async apiPostUsers(req,res,next){
+        try{
+            const userId = req.body.user_id
+            const name = req.body.name
+            const password = req.body.password
+            
+            const userResponse = await usersDAO.addUser(
+                userId,
+                name,
+                password
+            )
+            res.json({status: "success"})
+        }catch(e){
+            res.status(500).json({error: e.message})
+        }
+    }
+
+    static async apiUpdateUsers(req,res,next){
+        try{
+            const userId = req.body.user_id
+            const name = req.body.name
+            const password = req.body.password
+
+            const userResponse = await usersDAO.updateUser(
+                req.body.user_id,
+                userId,
+                name,
+                password
+            )
+
+            var { error } = userResponse
+            if(error){
+                res.status(400).json({error})
+            }
+            if(userResponse.modifiedCount === 0){
+                throw new Error(
+                    "unable to update user"
+                )
+            }
+            res.json({status: "success"})
+        }catch(e){
+            res.status(500).json({error: e.message})
+        }
+    }
+
+    static async apiDeleteUsers(req,res,next){
+        try{
+            const userId = req.body.user_id
+            console.log(userId)
+            const userResponse = await usersDAO.deleteUser(
+                userId
+            )
+            res.json({status: "success"})
+        }catch(e){
+            res.status(500).json({error: e.message})
+        }
     }
 }
--- backend/api/users.routes.js
+++ backend/api/users.routes.js
@@ -5,4 +5,10 @@ const router = express.Router()
 
 router.route("/").get(UsersCtrl.apiGetUsers)
 
+router
+    .route("/user")
+    .post(UsersCtrl.apiPostUsers)
+    .put(UsersCtrl.apiUpdateUsers)
+    .delete(UsersCtrl.apiDeleteUsers)
+
 export default router
