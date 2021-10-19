08ab8b9b3b34f18aa4c652b5c039d3e3d8e6a4f7
Yared <yareddeltoro@gmail.com>
2021-10-18 22:38:16 -0500

    movie test

--- backend/routes/movies.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const router = express.Router();
-
-router.route("/").get((req,res) => res.send("Main"))
-router.route("/movies").get((req,res) => res.send("Movies"))
-
-export default router
